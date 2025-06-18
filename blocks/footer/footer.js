6fc72e83c01a0e8f302a6313d3f218e77efb96e1
Vignesh Narendran <del40114@adobe.com>
2025-06-18 14:44:47 +0530

    adding space for if

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -38,7 +38,7 @@ export default async function decorate(block) {
   block.append(footer);
 
   waitForElement(block, '.consent-screen', (consentScreen) => {
-    if(consentScreen && localStorage.getItem('dcit_ca') == null) {
+    if (consentScreen && localStorage.getItem('dcit_ca') == null) {
       consentScreen.style.display = 'block';
       const acceptBtn = consentScreen.querySelector('.button[href="#acceptcookies"]');
       const denyBtn = consentScreen.querySelector('.button[href="#denycookies"]');
