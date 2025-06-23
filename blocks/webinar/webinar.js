2c093f1c915bccf9a3e2b04639a3b6e702313065
Bhosle <pbhosle@deloitte.com>
2025-06-23 18:54:36 +0530

    webinar otp funcitonality added

--- blocks/webinar/webinar.js
+++ blocks/webinar/webinar.js
@@ -49,7 +49,7 @@ export default async function decorate(block) {
 
   const getOtpBtn = document.createElement('button');
   getOtpBtn.type = 'button';
-  getOtpBtn.textContent = `Send otp to mobile`;
+  getOtpBtn.textContent = `Send otp to Mobile`;
   getOtpBtn.className = 'get-otp-btn';
   getOtpBtn.disabled = true;
 
