8fc220791a57d7810d4ebe49dd99a7c8b13e99e0
Thangapandi <athangapandi@deloitte.com>
2025-06-25 20:13:09 +0530

    Policy Form Util function and component creatioon

--- blocks/policy-form/policy-form.js
+++ blocks/policy-form/policy-form.js
@@ -1,11 +1,11 @@
-import { fetchPlaceholdersForLocale } from '../../scripts/scripts.js';
-import decoratePolicy from './policy-form-utils.js';
+// import { fetchPlaceholdersForLocale } from '../../scripts/scripts.js';
+// import decoratePolicy from './policy-form-utils.js';
 
 export default async function decorate(block) {
-  const formVal = block.children[0];
-  const placeholders = await fetchPlaceholdersForLocale();
+//   const formVal = block.children[0];
+//   const placeholders = await fetchPlaceholdersForLocale();
   console.log(block, 'block');
 
-  const form = decoratePolicy(formVal, placeholders);
-  block.append(form);
+//   const form = decoratePolicy(formVal, placeholders);
+//   block.append(form);
 }
