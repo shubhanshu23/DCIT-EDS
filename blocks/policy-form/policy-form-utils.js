e591c39c6e0d2bcfe9ec6e09bd09f35eecd82754
Thangapandi <athangapandi@deloitte.com>
2025-06-26 11:51:29 +0530

    Policy renewal and rejoin flow eith OTP and survey functionality implementaion

--- blocks/policy-form/policy-form-utils.js
+++ blocks/policy-form/policy-form-utils.js
@@ -60,5 +60,20 @@ export default function decoratePolicy(formLoad, placeholders) {
     form.append(emailInput, otpWrapper, submitBtn);
   }
 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent actual form submission
+    const locale = document.querySelector('meta[name="locale"]')?.content || 'en';
+
+    const policyNumber = policyInput?.value?.trim() || null;
+    const email = emailInput.value.trim();
+
+    // Here you can handle the form data, for example:
+    console.log('Form Submitted:', {
+      policyNumber,
+      email,
+    });
+    window.location.href = `/${locale}/policy-renewal-survey`;
+  });
+
   return form;
 }
